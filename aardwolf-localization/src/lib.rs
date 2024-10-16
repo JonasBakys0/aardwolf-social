0ddeeb7a519d773cfc284045c77cb06b40407fea
Banjo Fox <4459812+BanjoFox@users.noreply.github.com>
2024-10-16 11:41:58 -0400

    Add the `rust-i18n` crate to ructe-based templates. (#340)

--- aardwolf-localization/src/lib.rs
+++ aardwolf-localization/src/lib.rs
@@ -1,10 +1,41 @@
-// Load macro and init translations.
-// https://crates.io/crates/rust-i18n
-
+///-
+// Start of rust-i18n configuration
 // Load I18n macro, for allow you use `t!` macro in anywhere.
 #[macro_use]
 extern crate rust_i18n;
 
+// Init translations for current crate.
+// This will load Configuration using the `[package.metadata.i18n]` section in `Cargo.toml` if exists.
+// Or you can pass arguments by `i18n!` to override it.
+i18n!("locales");
+
 // Config fallback missing translations to "en" locale.
 // Use `fallback` option to set fallback locale.
-i18n!("locales", fallback = "en-us");
+//
+i18n!("locales", fallback = "en");
+
+// Or more than one fallback with priority.
+//
+i18n!("locales", fallback = ["en", "es"]);
+
+// Use a short hashed key as an identifier for long string literals
+// to optimize memory usage and lookup speed.
+// The key generation algorithm is `${Prefix}${Base62(SipHash13("msg"))}`.
+i18n!("locales", minify_key = true);
+//
+// Alternatively, you can customize the key length, prefix,
+// and threshold for the short hashed key.
+i18n!("locales",
+      minify_key = true,
+      minify_key_len = 12,
+      minify_key_prefix = "t_",
+      minify_key_thresh = 64
+);
+// Now, if the message length exceeds 64, the `t!` macro will automatically generate
+// a 12-byte short hashed key with a "t_" prefix for it, if not, it will use the original.
+
+// If no any argument, use config from Cargo.toml or default.
+i18n!();
+
+// End rust-i18n configuration
+///S
